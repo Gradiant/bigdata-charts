c53677e1584af5c594ce23e12a8b46ef99aeb0a2
Carlos Giraldo <cgiraldo@gradiant.org>
2020-03-03 13:37:50 +0100

    hdfs chart

---
+++ charts/hdfs/resources/config/bootstrap.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+: ${HADOOP_PREFIX:=/usr/local/hadoop}
+
+. $HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
+
+# Directory to find config artifacts
+CONFIG_DIR="/tmp/hadoop-config"
+
+# Copy config files from volume mount
+
+for f in slaves core-site.xml hdfs-site.xml mapred-site.xml yarn-site.xml; do
+    if [[ -e ${CONFIG_DIR}/$f ]]; then
+    cp ${CONFIG_DIR}/$f $HADOOP_PREFIX/etc/hadoop/$f
+    else
+    echo "ERROR: Could not find $f in $CONFIG_DIR"
+    exit 1
+    fi
+done
+
+# installing libraries if any - (resource urls added comma separated to the ACP system variable)
+cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
+if [[ $2 == "namenode" ]]; then
+    if [ ! -d "/dfs/name" ]; then
+    mkdir -p /dfs/name
+    $HADOOP_PREFIX/bin/hdfs namenode -format -force -nonInteractive
+    fi
+    $HADOOP_PREFIX/sbin/hadoop-daemon.sh start namenode
+fi
+if [[ $2 == "datanode" ]]; then
+    if [ ! -d "/dfs/data" ]; then
+    mkdir -p /dfs/data
+    fi
+    #  wait up to 30 seconds for namenode
+    (while [[ $count -lt 15 && -z `curl -sf http://{{ include "hdfs.fullname" . }}-namenode:50070` ]]; do ((count=count+1)) ; echo "Waiting for {{ include "hdfs.fullname" . }}-namenode" ; sleep 2; done && [[ $count -lt 15 ]])
+    [[ $? -ne 0 ]] && echo "Timeout waiting for hdfs namenode, exiting." && exit 1
+
+    $HADOOP_PREFIX/sbin/hadoop-daemon.sh start datanode
+fi
+if [[ $1 == "-d" ]]; then
+    until find ${HADOOP_PREFIX}/logs -mmin -1 | egrep -q '.*'; echo "`date`: Waiting for logs..." ; do sleep 2 ; done
+    tail -F ${HADOOP_PREFIX}/logs/* &
+    while true; do sleep 1000; done
+fi
+
+if [[ $1 == "-bash" ]]; then
+    /bin/bash
+fi
---
+++ charts/hdfs/templates/_helpers.tpl
@@ -0,0 +1,38 @@
+{{/* vim: set filetype=mustache: */}}
+{{/*
+Expand the name of the chart.
+*/}}
+{{- define "hdfs.name" -}}
+{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+
+{{/*
+Create a default fully qualified app name.
+We truncate at 54 chars because some Kubernetes name fields are limited to 63 (by the DNS naming spec),
+as we append -datanode or -namenode to the names
+If release name contains chart name it will be used as a full name.
+*/}}
+{{- define "hdfs.fullname" -}}
+{{- if .Values.fullnameOverride -}}
+{{- .Values.fullnameOverride | trunc 54 | trimSuffix "-" -}}
+{{- else -}}
+{{- $name := default .Chart.Name .Values.nameOverride -}}
+{{- if contains $name .Release.Name -}}
+{{- .Release.Name | trunc 54 | trimSuffix "-" -}}
+{{- else -}}
+{{- printf "%s-%s" .Release.Name $name | trunc 54 | trimSuffix "-" -}}
+{{- end -}}
+{{- end -}}
+{{- end -}}
+
+{{/*
+Standard Labels from Helm documentation https://helm.sh/docs/chart_best_practices/#labels-and-annotations
+*/}}
+
+{{- define "hdfs.labels" -}}
+helm.sh/chart: {{ .Chart.Name }}-{{ .Chart.Version | replace "+" "_" }}
+app.kubernetes.io/managed-by: {{ .Release.Service | quote }}
+app.kubernetes.io/instance: {{ .Release.Name | quote }}
+app.kubernetes.io/version: {{ .Chart.AppVersion | quote }}
+app.kubernetes.io/part-of: {{ .Chart.Name }}
+{{- end -}}
