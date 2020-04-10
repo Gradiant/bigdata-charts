3cc93dca4a3b5320d594aded459ac87fd32844c8
Michał Sochoń <kaszpir@gmail.com>
2020-04-10 21:51:00 +0200

    [opentsdb] Allow running custom hbase script, adjust logging
    * add logback.xml to output logs to console with different levels
    * add passing of env vars into the containers
    * change readiness probe so that it also checks connection to hbase
    * adjust hbase initialization script to allow using custom script
    * fix default compression for tables (LZO is not available in images)
    * init container prints rendered hbase script, easier for debugging
    * provide custom-values.yaml for testing customizations in ci
    * add extra test for hbase compression
    * update hbase initialization with additional options, especially TTL
    * keep vaules.yaml backward compatible
    * extend values.yaml with additional comments

--- charts/opentsdb/resources/config/create_hbase_tables.sh
+++ charts/opentsdb/resources/config/create_hbase_tables.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
-set -e
-COMPRESSION=GZ
+#!/bin/bash
+#set -x # increase verbosity
+set -e # exit on error
+set -a # export vars as env vars
 # Small script to setup the HBase tables used by OpenTSDB.
+# notice that this can be also set as ENV var inside of container image (as in dockerfile)
 test -n "$HBASE_PREFIX" || {
     echo >&2 'The environment variable HBASE_PREFIX must be set'
     exit 1
@@ -18,17 +20,41 @@ TREE_TABLE=${TREE_TABLE-'tsdb-tree'}
 META_TABLE=${META_TABLE-'tsdb-meta'}
 BLOOMFILTER=${BLOOMFILTER-'ROW'}
 # LZO requires lzo2 64bit to be installed + the hadoop-gpl-compression jar.
-COMPRESSION=${COMPRESSION-'LZO'}
+COMPRESSION=${COMPRESSION-'GZ'}
 # All compression codec names are upper case (NONE, LZO, SNAPPY, etc).
-COMPRESSION=`echo "$COMPRESSION" | tr a-z A-Z`
+COMPRESSION=${COMPRESSION^^}
+# DIFF encoding is very useful for OpenTSDB's case that many small KVs and common prefix.
+# This can save a lot of storage space.
+DATA_BLOCK_ENCODING=${DATA_BLOCK_ENCODING-'DIFF'}
+DATA_BLOCK_ENCODING=${DATA_BLOCK_ENCODING^^}
+# set 'time to live' in seconds for data, allows automatic data retention which will be handled by HBase itself
+TSDB_TTL=${TSDB_TTL-'FOREVER'}
 
 case $COMPRESSION in
-    (NONE|LZO|GZIP|SNAPPY)  :;;  # Known good.
+    (NONE|LZO|GZ|SNAPPY)  :;;  # Known good.
     (*)
     echo >&2 "warning: compression codec '$COMPRESSION' might not be supported."
     ;;
 esac
-echo "checking if opentsdb $UID_TABLE hbase table exists"
+# test if compression algorithm is supported by hbase installation (lowercase compression var)
+echo "Checking if given compression is supported..."
+$HBASE_PREFIX/bin/hbase org.apache.hadoop.hbase.util.CompressionTest file:///tmp/testfile ${COMPRESSION,,}
+
+case $DATA_BLOCK_ENCODING in
+  (NONE|PREFIX|DIFF|FAST_DIFF|ROW_INDEX_V1)  :;; # Know good
+  (*)
+    echo >&2 "warning: encoding '$DATA_BLOCK_ENCODING' might not be supported."
+    ;;
+esac
+
+# render hbase script to fill in env vars
+( echo "cat <<EOF" ; cat /tmp/init/hbase_script.txt ; echo ; echo EOF ) | bash > $HBASE_PREFIX/conf/hbase_script_parsed.txt
+echo "HBase script:"
+echo "----"
+cat $HBASE_PREFIX/conf/hbase_script_parsed.txt
+echo "----"
+
+echo "Checking if opentsdb $UID_TABLE hbase table exists"
 ret=$( echo "exists '$UID_TABLE'" | $HBASE_PREFIX/bin/hbase shell -n )
 if [[ $ret == *"true"* ]];
 then
@@ -36,19 +62,6 @@ then
     exit 0
 else
     echo "Creating OpenTSDB hbase tables:"
-    $HBASE_PREFIX/bin/hbase shell -n << EOF
-    create '$UID_TABLE',
-    {NAME => 'id', COMPRESSION => '$COMPRESSION', BLOOMFILTER => '$BLOOMFILTER'},
-    {NAME => 'name', COMPRESSION => '$COMPRESSION', BLOOMFILTER => '$BLOOMFILTER'}
-
-    create '$TSDB_TABLE',
-    {NAME => 't', VERSIONS => 1, COMPRESSION => '$COMPRESSION', BLOOMFILTER => '$BLOOMFILTER'}
-
-    create '$TREE_TABLE',
-    {NAME => 't', VERSIONS => 1, COMPRESSION => '$COMPRESSION', BLOOMFILTER => '$BLOOMFILTER'}
-
-    create '$META_TABLE',
-    {NAME => 'name', COMPRESSION => '$COMPRESSION', BLOOMFILTER => '$BLOOMFILTER'}
-EOF
-echo "DONE"
+    $HBASE_PREFIX/bin/hbase shell -n $HBASE_PREFIX/conf/hbase_script_parsed.txt
+    echo "DONE"
 fi
