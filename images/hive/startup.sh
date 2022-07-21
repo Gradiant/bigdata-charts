c9d1a91f14ca1585cb36fa0c672d7e035265d557
itzhang89 <itzhang89@163.com>
2022-07-21 22:59:37 +0800

    add a hive image from docker-hive repo

--- images/hive/startup.sh
+++ images/hive/startup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-hadoop fs -mkdir       /tmp
-hadoop fs -mkdir -p    /user/hive/warehouse
-hadoop fs -chmod g+w   /tmp
-hadoop fs -chmod g+w   /user/hive/warehouse
+hadoop fs -mkdir /tmp
+hadoop fs -mkdir -p /user/hive/warehouse
+hadoop fs -chmod g+w /tmp
+hadoop fs -chmod g+w /user/hive/warehouse
 
-cd $HIVE_HOME/bin
-./hiveserver2 --hiveconf hive.server2.enable.doAs=false
+$HIVE_HOME/bin/hiveserver2 --hiveconf hive.server2.enable.doAs=false
